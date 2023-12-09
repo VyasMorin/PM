bda014bdda22cc5c47895d4e16880a644f33af02
Leon <51339494+Blatko1@users.noreply.github.com>
2023-12-09 21:21:16 +0100

    dodao Rokov dio
    funkcije za računanje faktorijela brojeva preko petlje i stirlinga
    
    Signed-off-by: Leon <51339494+Blatko1@users.noreply.github.com>

--- main.cs
+++ main.cs
@@ -1,22 +1,34 @@
 using System;
 
-namespace lol{
-        class program{
+namespace Projekt {
+    class Program {
 
-        public static void Main(string[] args)
-        {
-       
-        //stirlingova formula
-                int x;
-            Console.WriteLine("Upisi broj: ");
-            x = Convert.ToInt32(Console.ReadLine());
-            double pi = 3.14;
-            double e = 2.71;
-            double stirling;
-            stirling = (Math.Sqrt(2 * pi * x)) * Math.Pow((x / e), x);
-            Console.WriteLine(stirling);
-                
+        public static int faktorijel_loop(int n) {
+            int faktorijel = 1;
+            for (int i = 2; i <= n; i++) {
+                faktorijel = faktorijel * i;
+            }
+            return faktorijel;
+        }
+
+        public static double faktorijel_stirling(double n) {
+            return Math.Sqrt(2.0 * Math.PI * n) * Math.Pow((n / Math.E), n);
+        }
+
+        public static void Main(string[] args) {
+
+            //stirlingova formula
+            Console.WriteLine("Upisi broj: \n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int faktorijel = Program.faktorijel_loop(n);
+            double stirling = Program.faktorijel_stirling(Convert.ToDouble(n));
+            double apsolutna_greska = Math.Abs(Convert.ToDouble(faktorijel) - stirling);
+
+            Console.WriteLine("'Klasicni' faktorijel broja " + n + " je " + faktorijel);
+            Console.WriteLine("Stirlingovom formulom faktorijel broja " + n + " je " + stirling);
+            Console.WriteLine("Apsolutna greska je " + apsolutna_greska);
+            Console.WriteLine("Relativna greska je " + (Math.Abs((faktorijel - stirling) / faktorijel) * 100) + "%");
+
         }
     }
 }
-    

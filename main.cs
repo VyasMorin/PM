6f33d17e4ba4526907083eb8560e77cd8eed703d
Freetula <123831512+Freetula@users.noreply.github.com>
2023-12-09 16:39:53 +0100

    Update main.cs
    Signed-off-by: Freetula <123831512+Freetula@users.noreply.github.com>

--- main.cs
+++ main.cs
@@ -8,7 +8,18 @@ namespace lol{
            
              Console.WriteLine("Upisi broj:");
              int number = Convert.ToInt32(Console.ReadLine());
-             Console.WriteLine("Tvoj broj: " + number);       
+             Console.WriteLine("Tvoj broj: " + number);     
+
+        //stirlingova formula
+                int x;
+            Console.WriteLine("Upisi broj: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            double pi = 3.14;
+            double e = 2.71;
+            double stirling;
+            stirling = (Math.Sqrt(2 * pi * x)) * Math.Pow((x / e), x);
+            Console.WriteLine(stirling);
+                
         }
     }
 }

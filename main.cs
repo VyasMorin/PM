c45f51db7621ffdd50a7baa1030731ceda8750f5
VyasMorin <103703803+VyasMorin@users.noreply.github.com>
2023-12-09 16:41:17 +0100

    Update main.cs
    Signed-off-by: VyasMorin <103703803+VyasMorin@users.noreply.github.com>

--- main.cs
+++ main.cs
@@ -5,11 +5,7 @@ namespace lol{
 
         public static void Main(string[] args)
         {
-           
-             Console.WriteLine("Upisi broj:");
-             int number = Convert.ToInt32(Console.ReadLine());
-             Console.WriteLine("Tvoj broj: " + number);     
-
+       
         //stirlingova formula
                 int x;
             Console.WriteLine("Upisi broj: ");

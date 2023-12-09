842b13e9c4edb4295f7d5fd2d3e8d9b3dadd9d95
Leon <51339494+Blatko1@users.noreply.github.com>
2023-12-09 22:06:33 +0100

    dodana rekurzivna funkcija - Roko
    Signed-off-by: Leon <51339494+Blatko1@users.noreply.github.com>

--- main.cs
+++ main.cs
@@ -7,7 +7,7 @@ namespace Projekt {
             if (n < 0) {
                 throw new ArgumentException("Input must be a non-negative integer.");
             }
-            
+
             long factorial = 1;
             for (int i = 1; i <= n; i++) {
                 factorial *= i;
@@ -23,16 +23,29 @@ namespace Projekt {
             return Math.Sqrt(2.0 * Math.PI * n) * Math.Pow((n / Math.E), n);
         }
 
+        static long CalculateFactorialRecursive(long n) {
+            if (n < 0) {
+                throw new ArgumentException("Input must be a non-negative integer.");
+            }
+            if (n == 0 || n == 1) {
+                return 1;
+            } else {
+                return n * CalculateFactorialRecursive(n - 1);
+            }
+        }
+
         public static void Main(string[] args) {
 
             //stirlingova formula
             Console.WriteLine("Upisi broj: \n");
             int n = Convert.ToInt32(Console.ReadLine());
             long faktorijel = Program.CalculateFactorialLoop(n);
+            long faktorijel2 = Program.CalculateFactorialRecursive(n);
             double stirling = Program.CalculateFactorialStirling(Convert.ToDouble(n));
             double apsolutna_greska = Math.Abs(Convert.ToDouble(faktorijel) - stirling);
 
             Console.WriteLine("'Klasicni' faktorijel broja " + n + " je " + faktorijel);
+            Console.WriteLine("'Klasicni' faktorijel broja " + n + " rekurzivnom funkcijom je " + faktorijel);
             Console.WriteLine("Stirlingovom formulom faktorijel broja " + n + " je " + stirling);
             Console.WriteLine("Apsolutna greska je " + apsolutna_greska);
             Console.WriteLine("Relativna greska je " + (Math.Abs((faktorijel - stirling) / faktorijel) * 100) + "%");
